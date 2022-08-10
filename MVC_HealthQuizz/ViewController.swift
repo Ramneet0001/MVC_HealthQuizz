fa3976a4b1f6829a40342311b7fb98aa263510e4
Ramneet <ramneet0001@yahoo.com>
2022-08-10 12:12:39 +0530

    Initial Commit

---
+++ MVC_HealthQuizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC_HealthQuizz
+//
+//  Created by Ramneet Singh on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
